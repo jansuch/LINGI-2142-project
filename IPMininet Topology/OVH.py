25a912e60ee7d215fea730f1fd44492766f7a834
jansuch <jan.suchara@gmail.com>
2020-10-22 14:30:39 +0200

    Update OVH.py

--- IPMininet Topology/OVH.py
+++ IPMininet Topology/OVH.py
@@ -9,64 +9,48 @@ from ipmininet.router.config import BGP, OSPF6, RouterConfig, AF_INET6, set_rr, 
 class MyTopology(IPTopo):
     """
     Please read me before digging in the code of this script.
-
     This simple network topology tries to connect two hosts separated
     by multiple routers and ASes.
-
     Running this network should be straightforward:
      i./ The script must be run as root since mininet will create routers inside your own machine
          $ chmod +x main.py
          $ sudo ./main.py
-
     ii./ The network should be started. The "mininet" CLI should appear on screen
     '''
     mininet>
     '''
-
     To access to one of the network, execute this command "xterm <your node name>". A new
     xterm terminal will be spawned. This new terminal will run bash. This means that you
     can execute any linux command. Be careful as the terminal is run as root!!
-
     '''
     mininet> xterm as1_rr1
     '''
-
     To access to the configuration of FRRouting, you have to use telnet to connect to
     FRRouting daemons.
     A different port is used to access to every routing daemon. This small table shows
     the port associated to its default daemon:
-
     PORT     STATE SERVICE
     2601/tcp open  zebra   --> controls the RIB of each daemon
     2605/tcp open  bgpd    --> show information related to the configuration of BGP
     2606/tcp open  ospf6d  --> same but for OSPFv3 (OSPF for IPv6)
-
     For example, if you want to look for all prefixes contained in the RIB, you must execute
     this command :
     <in the xterm of your node>$ telnet localhost 2601
-
     A new cli interface will be shown:
-
     '''
     Trying ::1...
     Connected to localhost
     Escape character is '^]'.
-
     Hello, this is FRRouting (version v7.4).
     Copyright 1996-2005 Kunihiro Ishiguro, et al.
-
     User Access Verification
-
     Password:
     '''
-
     At this time, you will be prompted for a password. In ipmininet the default password is "zebra".
     Simply type it and the FRRouting CLI will be shown:
-
     '''
     as1_rr1>
     '''
-
     Type "show ipv6 route" to show all the routes contained in the RIB. You can find an example of output below
     ''''
     as1_rr1> c
@@ -75,7 +59,6 @@ class MyTopology(IPTopo):
            v - VNC, V - VNC-Direct, A - Babel, D - SHARP, F - PBR,
            f - OpenFabric,
            > - selected route, * - FIB route, q - queued route, r - rejected route
-
     B>* c1a4:4ad:c0ff:ee::/64 [20/0] via fe80::f802:bbff:fe6d:4da0, as1_rr1-eth2, weight 1, 00:50:34
     O>* cafe:babe:dead:beaf::/64 [110/2] via fe80::2c5c:4ff:fe4a:2b73, as1_rr1-eth1, weight 1, 00:50:30
     O   fc00:0:3::/48 [110/1] is directly connected, as1_rr1-eth0, weight 1, 00:50:35
@@ -101,19 +84,14 @@ class MyTopology(IPTopo):
     C * fe80::/64 is directly connected, as1_rr1-eth2, 00:50:38
     C>* fe80::/64 is directly connected, as1_rr1-eth1, 00:50:38
     '''
-
     Press CTRL + D to exit the session. And again to exit the xterm session.
-
     You can find more information on how to use the CLI of FRRouting daemons in the FRRouting DOCS:
     http://docs.frrouting.org/en/latest/
-
     Remember that xterm launches a root bash. You can run any executable (in ROOT!).
     If wireshark is installed on your computer, you can execute it to capture packets
     reaching interfaces of your mininet node.
-
     The same applies if you want to check the Linux FIB (ip addr) or the addresses attached to the node
     interfaces (ip route)
-
     Finally, you can find other details on how to build an ipmininet script here:
     https://ipmininet.readthedocs.io/en/latest/
     """
@@ -189,7 +167,14 @@ class MyTopology(IPTopo):
                 rbx_g1_nc5,rbx_g2_nc5,
                 par_gsw_sbb1_nc5,par_th2_sbb1_nc5]
 	
-        self.addiBGPFullMesh(1, routers=routers)
+        set_rr(self, rr=rbx_g2_nc5, peers=[par_gsw_sbb1_nc5, par_th2_sbb1_nc5,lon_thw_sbb1_nc5, lon_drch_sbb1_nc5])
+        set_rr(self, rr=rbx_g1_nc5, peers=[par_gsw_sbb1_nc5, par_th2_sbb1_nc5,lon_thw_sbb1_nc5, lon_drch_sbb1_nc5])
+        set_rr(self, rr=gra_g1_nc5, peers=[gra_g2_nc5, rbx_g1_nc5, rbx_g2_nc5,fra_fr5_sbb1_nc5,fra_fr5_sbb2_nc5])
+        set_rr(self, rr=gra_g2_nc5, peers=[gra_g1_nc5, rbx_g1_nc5, rbx_g2_nc5,fra_fr5_sbb1_nc5,fra_fr5_sbb2_nc5])
+        set_rr(self, rr=fra_fr5_sbb1_nc5, peers=[fra_1_n7, fra_5_n7])
+        set_rr(self, rr=fra_fr5_sbb2_nc5, peers=[fra_1_n7, fra_5_n7])
+
+        #self.addiBGPFullMesh(1, routers=routers)
         self.addAS(16276, routers=routers)
 
         # adding BGP to establish iBGP sessions
